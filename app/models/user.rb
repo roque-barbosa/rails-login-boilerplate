27c84ab27db3829906817ff27a04b54a6c2e94f8
Roque Barbosa <roquecostajr@outlook.com>
2023-11-19 22:13:57 -0300

    feat: change model to use revogaton strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # Make model use the revocation strategy 
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   enum role:  %i(client worker admin)
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
 end
